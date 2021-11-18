de555771b6be9748177a45f79cb81ae0bed7d7f9
Doan_Hung_168 <hung0868358175@gmail.com>
2021-11-19 00:25:11 +0700

    thêm phần sản phẩm yêu thích

---
+++ app/src/main/java/com/nhomduan/quanlyungdungdathang/Adapter/FavoriteProductAdapter.java
@@ -0,0 +1,129 @@
+package com.nhomduan.quanlyungdungdathang.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.nhomduan.quanlyungdungdathang.Activity.HomeActivity;
+import com.nhomduan.quanlyungdungdathang.Activity.ShowProductActivity;
+import com.nhomduan.quanlyungdungdathang.Model.Product;
+import com.nhomduan.quanlyungdungdathang.R;
+import com.nhomduan.quanlyungdungdathang.Utils.ProductUtils;
+import com.nhomduan.quanlyungdungdathang.Utils.UserUtils;
+import com.squareup.picasso.Picasso;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class FavoriteProductAdapter extends RecyclerView.Adapter<FavoriteProductAdapter.ViewHolder> {
+
+    Context context;
+    List<Product> list;
+
+    public FavoriteProductAdapter(Context context, List<Product> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_san_pham_yeu_thich,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Product product = list.get(position);
+        Picasso.get()
+                .load(product.getImage())
+                .placeholder(R.drawable.ic_image)
+                .into(holder.imgProduct);
+        holder.tvNameProduct.setText(product.getName());
+        holder.tvTimeProduct.setText(product.getThoiGianCheBien() +" min");
+        holder.tvSoNguoiThichSP.setText(String.valueOf(product.getRate()));
+        holder.tvSoNguoiMuaSP.setText(String.valueOf(product.getSo_luong_da_ban()));
+
+        Locale locale = new Locale("vi", "VN");
+        NumberFormat currencyFormat = NumberFormat.getNumberInstance(locale);
+        holder.tvPriceProduct.setText(currencyFormat.format((int)(product.getGia_ban()-product.getGia_ban()*product.getKhuyen_mai()))+" VNĐ");
+
+        holder.viewHolderProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ShowProductActivity.class);
+                intent.putExtra("productId", product.getId());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imgProduct;
+        ToggleButton imgHearthLike;
+        TextView tvNameProduct,tvTimeProduct,tvPriceProduct;
+        public LinearLayout viewHolderProduct;
+
+        //
+        private TextView tvSoNguoiThichSP;
+        private TextView tvSoNguoiMuaSP;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgProduct = itemView.findViewById(R.id.imgProduct);
+            tvNameProduct = itemView.findViewById(R.id.tvNameProduct);
+            tvTimeProduct = itemView.findViewById(R.id.tvTimeProduct);
+            viewHolderProduct = itemView.findViewById(R.id.viewHolderProduct);
+            tvPriceProduct = itemView.findViewById(R.id.tvPriceProduct);
+            tvSoNguoiThichSP = itemView.findViewById(R.id.tvSoNguoiThichSP);
+            tvSoNguoiMuaSP = itemView.findViewById(R.id.tvSoNguoiMuaSP);
+        }
+    }
+
+    public void removeItem(Product product,int index) {
+        List<String> maSanPhamYeuThich = HomeActivity.userLogin.getMa_sp_da_thich();
+        int viTri = -1;
+        for(int i = 0; i < maSanPhamYeuThich.size(); i++) {
+            if(maSanPhamYeuThich.get(i).equals(product.getId())) {
+                viTri = i;
+            }
+        }
+        maSanPhamYeuThich.remove(viTri);
+        UserUtils.getDbRefUser().child(HomeActivity.userLogin.getId()).child("ma_sp_da_thich")
+                .setValue(maSanPhamYeuThich);
+        ProductUtils.getDbRfProduct().child(product.getId()).child("rate")
+                .setValue(product.getRate() - 1);
+        list.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    public void undoItem(Product product,int index) {
+        List<String> maSanPhamYeuThich = HomeActivity.userLogin.getMa_sp_da_thich();
+        maSanPhamYeuThich.add(product.getId());
+        UserUtils.getDbRefUser().child(HomeActivity.userLogin.getId()).child("ma_sp_da_thich")
+                .setValue(maSanPhamYeuThich);
+        ProductUtils.getDbRfProduct().child(product.getId()).child("rate")
+                .setValue(product.getRate() + 1);
+        list.add(index, product);
+        notifyItemInserted(index);
+    }
+}
--- app/src/main/java/com/nhomduan/quanlyungdungdathang/Fragment/LikeProductFragment.java
+++ app/src/main/java/com/nhomduan/quanlyungdungdathang/Fragment/LikeProductFragment.java
@@ -1,21 +1,155 @@
 package com.nhomduan.quanlyungdungdathang.Fragment;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.nhomduan.quanlyungdungdathang.Activity.HomeActivity;
+import com.nhomduan.quanlyungdungdathang.Adapter.FavoriteProductAdapter;
+import com.nhomduan.quanlyungdungdathang.Interface.ItemTouchHelpListener;
+import com.nhomduan.quanlyungdungdathang.Model.Product;
 import com.nhomduan.quanlyungdungdathang.R;
+import com.nhomduan.quanlyungdungdathang.Utils.ProductUtils;
+import com.nhomduan.quanlyungdungdathang.Utils.RecyclerViewItemTouchHelper;
 
-public class LikeProductFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LikeProductFragment extends Fragment implements ItemTouchHelpListener {
+
+    private HomeActivity activity;
+    private FragmentManager fragmentManager;
+    private Toolbar toolbar;
+    private RecyclerView rcvSanPhamYeuThich;
+    private LinearLayout viewRoot;
+
+
+
+
+    private List<Product> productList;
+    private FavoriteProductAdapter favoriteProductAdapter;
+
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_like_product, container, false);
+        return inflater.inflate(R.layout.fragment_like_product, container, false);
+    }
 
-        return view;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        Log.e("dsf", "fsdf");
+        activity.setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerViewHome, new HomeFragment())
+                        .commit();
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        setUpListSPYeuThich();
+    }
+
+    private void setUpListSPYeuThich() {
+        productList = new ArrayList<>();
+        favoriteProductAdapter = new FavoriteProductAdapter(getContext(), productList);
+        rcvSanPhamYeuThich.setLayoutManager(new LinearLayoutManager(getContext()));
+        rcvSanPhamYeuThich.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        rcvSanPhamYeuThich.setAdapter(favoriteProductAdapter);
+
+        ItemTouchHelper.SimpleCallback simpleCallback =
+                new RecyclerViewItemTouchHelper(0, ItemTouchHelper.LEFT, this);
+        new ItemTouchHelper(simpleCallback).attachToRecyclerView(rcvSanPhamYeuThich);
+
+
+        List<String> list = HomeActivity.userLogin.getMa_sp_da_thich();
+        if(list != null) {
+            for(String maSP : list) {
+                ProductUtils.getDbRfProduct().child(maSP).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @SuppressLint("NotifyDataSetChanged")
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if(task.isSuccessful() && task.getResult() != null) {
+                            Product product = task.getResult().getValue(Product.class);
+                            if(product != null) {
+                                productList.add(product);
+                                favoriteProductAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    private void initView(View view) {
+        toolbar = view.findViewById(R.id.toolbar);
+        rcvSanPhamYeuThich = view.findViewById(R.id.rcvSanPhamYeuThich);
+        activity = (HomeActivity) requireActivity();
+        fragmentManager = getParentFragmentManager();
+        viewRoot = view.findViewById(R.id.viewRoot);
+
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder) {
+        if(viewHolder instanceof FavoriteProductAdapter.ViewHolder) {
+            String productName = productList.get(viewHolder.getAdapterPosition()).getName();
+
+            final Product product = productList.get(viewHolder.getAdapterPosition());
+            final int indexDelete = viewHolder.getAdapterPosition();
+
+            // remove item ;
+            favoriteProductAdapter.removeItem(product,indexDelete);
+            Snackbar snackbar = Snackbar.make(viewRoot, productName + " removed !", Snackbar.LENGTH_LONG);
+            snackbar.setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    favoriteProductAdapter.undoItem(product, indexDelete);
+                    if(indexDelete == 0 || indexDelete == productList.size() - 1) {
+                        rcvSanPhamYeuThich.scrollToPosition(indexDelete);
+                    }
+                }
+            });
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.show();
+        }
     }
 }
